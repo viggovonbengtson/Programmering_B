b8758b6f5e5d955ead6d81d4ede9fcda2c9b1fb7
viggovonbengtson <viggovonbengtson@gmail.com>
2026-03-21 20:28:48 +0100

    Har oprettet en ny hjemmeside til at fumle rundt med lidt hit og her. Og en hel masse andre ting også

---
+++ Z_test_website/index.js
@@ -0,0 +1,49 @@
+
+
+var currentPage = "#page1"
+var videoButton, theVideo
+var videoPlaying = true
+
+
+//P5 setup bliver kaldt EN gang før siden vises
+function setup()
+{
+    console.log('P5 setup kaldt')
+
+    //skift til current page
+    shiftPage(currentPage)
+    
+    
+    select('#nekstPageButton').mousePressed(() => {
+        shiftPage("#page2")
+    })
+    
+    
+    //sæt menu op
+    //hent alle sider som et array
+    var allPages = selectAll('.page')
+    //løb listen igennem en for en
+    allPages.map(
+        (page) => {
+            //lav et nyt <a> element
+            var menuItem = createElement('a')
+            //sæt 'a' tagget's html til sidens titel
+            menuItem.html(page.attribute('title'))
+            //sæt eventlister på 'a' tagget
+            menuItem.mousePressed(
+                () => shiftPage('#' + page.attribute('id'))
+            )
+            //sæt tagget ind i sidebar
+            select('.sidebar').child(menuItem)
+        }
+    )
+}
+
+
+
+function shiftPage(newPage)
+{
+    select(currentPage).removeClass('show')
+    select(newPage).addClass('show')
+    currentPage = newPage
+}
