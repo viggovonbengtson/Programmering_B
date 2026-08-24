509b0c4a8d8c16a0675454224dbe0dd184cc89fb
viggovonbengtson <viggovonbengtson@gmail.com>
2026-08-24 15:27:41 +0200

    har lavet videre på two player spil, og har rykket mit API ind i ydre filer så ALLE projekter kan tage fat i dem

--- 15_DATA_ALGORITMER/controller/index.js
+++ 15_DATA_ALGORITMER/controller/index.js
@@ -1,3 +1,24 @@
+
+//array med alle Rick and Morty karakterene
+var chars = []
+
 function setup() {
     // Bind controllerens knapper og send handlinger over MQTT her.
+    getChars()
+    
+    
+}
+
+async function getChars(){
+    //vi starter med at hente karakterene i Rick and Morty API
+    var res
+    try{
+        var res = await fetch('https://rickandmortyapi.com/api/character?page=1')
+    }catch(err){
+        console.log(err)
+    }
+    //Hvis response er ok, henter vi json data
+    var json = await res.json()
+    chars = json.results
+    console.log(chars)
 }
