b1af49af02ed878a345901e4d376c914e485cfc8
viggovonbengtson <viggovonbengtson@gmail.com>
2026-09-13 12:22:01 +0200

    har lavet bold-spillet, og skal nu til at lave lave lektien som ligger på studica

--- 16_canvas_og_OOP/14_canvas_og_OOP/index.js
+++ 16_canvas_og_OOP/14_canvas_og_OOP/index.js
@@ -1,67 +1,57 @@
-var x = 20
-var ballOneSpeed = 10
-var r = 40
-
-var hSpeed
-var g
-var position
-var veloctiy
-var radius = 20
-var friction = 0.99
+var gravity
+var friction
+var b
+var f
+var points = 1000
+var bSound
 
 
+async function setup() {
 
-//P5 setup() bliver kaldt EN gang før siden vises 
-function setup(){
-    var canvas = createCanvas(windowWidth, windowHeight)
-    canvas.parent('#page1')
-
-    position.x = windowWidth/2
+    bSound = await loadSound("/api_lib/sfx/cat-choke.mp3")
+    var c = createCanvas(windowWidth, windowHeight)
+    select("#page2").child(c)
+    select("#startButton").mousePressed(()=>shiftPage("#page2"))
+    gravity = createVector(0, 1)
+    friction = 0.97
+    
+    
+    b = new Ball(windowWidth/2, 0, 10, "orange", 12)
+    f = new FloatingBall(100, 100, 50, "red", 0, 20)
+    
 }
 
-
-//draw kører 60x i sekundet(hvis framerate er 60fps)
 function draw() {
-    //vi kan sltte draw frammerate her
-    frameRate(60)
-
-    background(220, 100, 50)
-
-    g = createVector(0, 1)
-    veloctiy = createVector(0, 0.9)
-    hSpeed = createVector(0, 5)
-
-    //håndtér kugle 1
-    fill('lightblue')
-    circle(x, 100, r)
+    background(100);
+    b.update()
+    b.constrain()
+    b.show()
     
-    x = x + ballOneSpeed
     
-    if(x > windowWidth - r/2 || x < 0 + r/2){
-        ballOneSpeed = -ballOneSpeed
+    if(b.hit(f)){
+        points--
+        bSound.play()
     }
     
-    // håndtér kugle 2
-    fill('lime')
-    noStroke()
-    //fald ned mod "jorden"
-    veloctiy += g
-    veloctiy *= friction
-    position.y += veloctiy
-
-    circle(position.x, position.y, radius)
-
-    if(position.y >= windowHeight - radius/2){
-        position.y = windowHeight-radius/2
-        veloctiy = -veloctiy
-    }
-
-    select('#info').html(round(veloctiy, 2))
+    
+    select('#info').html(points)
+    
+    
+    f.update()
+    f.constrain()
+    f.show()
 }
 
-function keyPressed(){
-    console.log(key)
-    if(key == " "){
-        veloctiy += -10
+function keyPressed() {
+    if(key == "w"){
+        b.jump()
+        f.jump()
+        b.diam += 10
+    }
+    if(key == "s" && b.diam >=21){
+        b.diam -= 20
+    }
+    if(key == "r"){
+        b.diam = 10
     }
 }
---
+++ api_lib/myClasses.js
@@ -0,0 +1,61 @@
+
+
+class Ball{
+  constructor(x, y, r, col, jump){
+    this.diam = r
+    this.col = col
+    this.velocity = createVector(0,0)
+    this.position = createVector(x,y)
+    this.jumpForce = jump
+  }
+  update(){
+    this.velocity.add(gravity)
+    this.velocity.y *= friction 
+    this.position.add(this.velocity)
+  }
+  constrain(){
+    if(this.position.y > height - this.diam/2){
+      this.position.y = height -this.diam/2
+      this.velocity.y *= -1
+    }
+  }
+  jump(){
+    this.velocity.y -= this.jumpForce
+  }
+  
+  show(){
+    fill(this.col)
+    circle(this.position.x, this.position.y, this.diam)
+  }
+  hit(secondBall){
+    var b = secondBall
+    var totalR = (this.diam + b.diam) /2
+    var d = dist(this.position.x, this.position.y, b.position.x, b.position.y)
+    if(d <= totalR){
+        console.log('Balls Hit')
+        return true
+    }else{
+        return false
+    }
+  }
+}
+
+class FloatingBall extends Ball{
+    constructor(x, y, r, col, jump, speed){
+        // super betyder at vi overtager disse argumenter fra "super" klassen (ball)
+        super(x, y, r, col, jump)
+        //vi overskriver velocity  vektoren med en lokal der flytter sig på x-aksen
+        this.velocity = createVector(speed, 0)
+    }
+    update(){
+        this.position.add(this.velocity)
+    }
+
+    constrain(){
+        // sørg for at FloatingBall bouncer på siderne
+        this.position.x = constrain(this.position.x, this.diam/2, windowWidth - this.diam/2)
+        if(this.position.x <= this.diam/2 || this.position.x >= windowWidth - this.diam/2){
+            this.velocity.x *= -1
+        }
+    }
+}
