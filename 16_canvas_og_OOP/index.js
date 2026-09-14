68f782dc181c57586a1eadc3b3e95c6b253dbe84
viggovonbengtson <viggovonbengtson@gmail.com>
2026-09-14 12:35:15 +0200

    sound hotfixx igeennn

--- 16_canvas_og_OOP/index.js
+++ 16_canvas_og_OOP/index.js
@@ -16,7 +16,7 @@ var hasPlayed = false; //styrer at lyden kun spillet EN GANG
 async function setup() {
 
     bSound = await loadSound("../api_lib/sfx/cat-choke.mp3")
-    jSound = await loadSound('/16_canvas_og_OOP/sfx/bass.mp3')
+    jSound = await loadSound('sfx/bass.mp3')
     var c = createCanvas(windowWidth, windowHeight)
     gravity = createVector(0, 1)
     friction = 0.97
